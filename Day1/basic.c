d54a0a4b5dda432782cff3ae08095aca79988f87
AmarKarande <07122001amar@gmail.com>
2024-12-31 12:05:46 +0530

    temp

---
+++ Day1/basic.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+    printf("Hello World! \n");
+    // Integer variable declaration
+    int marks=20;
+    printf("marks = %d\n",marks);
+
+    // Float variable declaration
+    float mark=35.10;
+    printf("mark=%f\n",mark);
+
+    // Character variable declaration
+    char name= 'A';
+    printf("name= %c \n",name);
+
+    double d=20.20;
+    printf("d =%lf\n", d);
+
+// short int variable
+    short int n1=12;
+    printf("n1 = %hd \n",n1);
+
+  
+
+    // unsigned int variable
+    unsigned int n2=123;
+    printf("n2= %u \",n2);
+
+    
+      return 0;
+}
---
+++ Day2/logical_operators.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    
+    int a=10;
+    int b=20;
+    int c=10;
+    // Logical and== &&
+    printf("a<b && a<b = %d \n", a>b && a<b);
+    printf("a==c && a<c = %d \n",a==c && a<b);
+
+    // Logical or  ||
+    printf(" a>b || b<c =  %d \n", a>b || b<c);
+    printf("a<b || c>b =  %d \n",a<b || c>b);
+
+    // Logical not ! change true= flase and false= true
+    printf("!(a>b) = %d \n",!(a>b));
+     printf("!(a<b && a<b = %d \n"), !(a>b && a<b) ;
+
+
+
+    return 0;
+}
